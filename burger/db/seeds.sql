b0db93aa64d6140782574810f844617fa7c88bc8
Ruby Dease <rubydease@me.com>
2017-09-03 17:09:17 -0400

    Delete seeds.sql

--- burger/db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO burger (burger_name, devoured, date) VALUES ('American Crunch', false);
-INSERT INTO burger (burger_name, devoured, date) VALUES ('All the way', false);
-INSERT INTO burger (burger_name, devoured, date) VALUES ('Cantina burger', flase);
-INSERT INTO burger (burger_name, devoured, date) VALUES ('Bacon Burger', false);
-INSERT INTO burger (burger_name, devoured, date) VALUES ('Zacks Special', false);
-INSERT INTO burger (burger_name, devoured, date) VALUES ('crepe cellar', false);
-
-SELECT * FROM burger
